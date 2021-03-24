b28dab24cd7a808602de060d334f07320c1087ab
yan00105 <yan00105@algonquinlive.com>
2021-03-23 23:04:03 -0400

    Android Project01: MainActivity update:3-23 4 button cardatabase

--- app/src/main/java/com/cst2335/project01/CarActivity.java
+++ app/src/main/java/com/cst2335/project01/CarActivity.java
@@ -3,6 +3,9 @@ package com.cst2335.project01;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -14,79 +17,44 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class CarActivity extends AppCompatActivity {
+//    at least 1 Toast, Snackbar, and AlertDialog notification.
 
-    MyListAdapter myAdapter;
-    SQLiteDatabase db;
-    EditText eText;
-    ListView listView;
-    Button btnTrivia;
-    Button btnSongster;
-    Button btnCardata;
-    Button btnSoccer;
+    EditText companyName;
 
-    private ArrayList<Message> elements = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_database);
 
+        companyName = findViewById(R.id.companyName);
 
-        myAdapter = new MyListAdapter();
-        ListView myList = (ListView) findViewById(R.id.ListView);
-        myList.setAdapter(myAdapter = new MyListAdapter());
-        eText = findViewById(R.id.eText);
+        SharedPreferences prefs = getSharedPreferences("manufacturerName", Context.MODE_PRIVATE);
+        String saveString = prefs.getString("ReserveName","");
+        companyName.setText(saveString);
 
-        listView = findViewById(R.id.listView);
-        btnCardata = findViewById(R.id.btnCardata);
+        Intent goToSearch = new Intent(CarActivity.this, SearchActivity.class);
 
-        btnCardata.setOnClickListener( bt->{
-//            startActivity(goToCar);
+        Button searchBtn = findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String savedString = companyName.getText().toString();
+                goToSearch.putExtra("typeEmails",savedString);
+                startActivity(goToSearch);
+            }
         });
-
-        protected void showContact(int position)
-        {
-            Message selectedContact = elements.get(position);
-            View extraStuff = getLayoutInflater().inflate(R.layout.empyty, null);
-            //get the TextViews
-            EditText rowMsg = extraStuff.findViewById(R.id.eText);
-//        EditText rowSend = extraStuff.findViewById(R.id.sBtn);
-            TextView rowId = extraStuff.findViewById(R.id.row_id);
-
-            //set the fields for the alert dialog
-//eText.setText(selectedContact.getMessage());
-//        rowSend.setText(selectedContact.getIsSend());
-            rowMsg.setText(selectedContact.getMessage());
-            rowId.setText("id:" + selectedContact.getId());
-
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("You clicked on item #" + position)
-
-                    //What is the message:
-                    .setMessage("The selected row is: " + (position + 1) +
-                            "\n" +
-                            "You can update the fields and then click update to save in the database")
-                    // add extra layout elements:showing the contact information
-                    .setView(extraStuff)
-                    //what the update button does:
-                    .setPositiveButton("Update", (click, arg) -> {
-//                        elements.add("HELLO");
-                        selectedContact.update(rowMsg.getText().toString());
-                        updateContact(selectedContact);
-                        myAdapter.notifyDataSetChanged();
-                    })
-                    //What the delete button does:
-                    .setNegativeButton("Delete", (click, arg) -> {
-                        deleteContact(selectedContact); //remove the contact from database
-                        elements.remove(position); //remove the contact from contact list
-                        myAdapter.notifyDataSetChanged(); //there is one less item so update the list
-
-                    })
-
-//                    An optional third button:
-                    .setNeutralButton("Dissmiss", (click, arg) -> {  })
-
-                    //Show the dialog
-                    .create().show();
-        }
     }
+
+    private void saveSharedPrefs(String stringToSave) {
+        SharedPreferences prefs = getSharedPreferences("manufacturerName", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("ReserveName", stringToSave);
+        editor.commit();
+    }
+    public void onPause() {
+        super.onPause();  // Always call the superclass method first
+        saveSharedPrefs(companyName.getText().toString());
+    }
+
 }
