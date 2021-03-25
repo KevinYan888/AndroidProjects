642d9b01e336fc93472e50e050ece2020a78ce84
yan00105 <yan00105@algonquinlive.com>
2021-03-25 00:54:27 -0400

    Android Project01: MainActivity update:3-23 4 button cardatabase

--- app/src/main/java/com/cst2335/project01/SearchActivity.java
+++ app/src/main/java/com/cst2335/project01/SearchActivity.java
@@ -3,6 +3,7 @@ package com.cst2335.project01;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -19,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,6 +33,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -65,24 +68,64 @@ public class SearchActivity extends AppCompatActivity {
         EditText searchEdit = findViewById(R.id.searchEdit);
         searchEdit.setText(fromCarData.getStringExtra("manufacturerName"));
 
-        myAdapter = new MyListAdapter();
-        ListView myList = (ListView) findViewById(R.id.sListView);
-        myList.setAdapter(myAdapter = new MyListAdapter());
-        searchEdit = findViewById(R.id.searchEdit);
-
-        myList.setAdapter(myAdapter);
-
-
-
-//        modelID = findViewById(R.id.modelID);
-        modelName = findViewById(R.id.modelName);
-//        progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar);
 //        progressBar.setVisibility(View.VISIBLE );
 
         SearchCar sCar = new SearchCar();//obj
-        sCar.execute("https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/Honda?format=JSON",
-                "https://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");
-    }//Type 1
+        sCar.execute("https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/"+fromCarData.getStringExtra("manufacturerName")+"?format=JSON");//Type 1
+
+        myAdapter = new MyListAdapter();
+        ListView myList = (ListView) findViewById(R.id.sListView);
+
+        myList.setAdapter(myAdapter = new MyListAdapter());
+
+        myList.setOnItemLongClickListener((parent, view, pos, id) -> {
+            showContact( pos );
+            return true;
+        });
+    }
+
+
+    protected void showContact(int position)
+    {
+        Object selectedContact = list.get(position);
+        View extraStuff = getLayoutInflater().inflate(R.layout.search_list, null);
+        //get the TextViews
+        TextView modelName = extraStuff.findViewById(R.id.modelName);
+
+        TextView modelId = extraStuff.findViewById(R.id.modelID);
+
+        modelName.setText(selectedContact.toString());
+        modelId.setText(selectedContact.toString());
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("You clicked on item #" + position)
+
+                //What is the message:
+                .setMessage("The selected row is: " + (position + 1) +
+                        "\n" +
+                        "You can update the fields and then click update to save in the database")
+                // add extra layout elements:showing the contact information
+                .setView(extraStuff)
+                //what the update button does:
+                .setPositiveButton("Update", (click, arg) -> {
+//                        elements.add("HELLO");
+//                    selectedContact.update(rowMsg.getText().toString());
+//                    updateContact(selectedContact);
+                    myAdapter.notifyDataSetChanged();
+                })
+                //What the delete button does:
+                .setNegativeButton("Delete", (click, arg) -> {
+//                    deleteContact(selectedContact); //remove the contact from database
+                    list.remove(position); //remove the contact from contact list
+                    myAdapter.notifyDataSetChanged(); //there is one less item so update the list
+                })
+//                    An optional third button:
+                .setNeutralButton("Dissmiss", (click, arg) -> {  })
+                //Show the dialog
+                .create().show();
+    }
+
 
     private class SearchCar extends AsyncTask< String, Integer, String>
     {
@@ -98,10 +141,10 @@ public class SearchActivity extends AppCompatActivity {
 
                 //open the connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                Log.i("MainActivity", "000000000000000000" ) ;
+
                 //wait for data:
                 InputStream response = urlConnection.getInputStream();
-                Log.i("MainActivity", "111111111111111111111" ) ;
+
                 //JSON reading:   Look at slide 26
                 //Build the entire string response:
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
@@ -109,81 +152,55 @@ public class SearchActivity extends AppCompatActivity {
 
                 String line = null;
                 if((line = reader.readLine()) != null){
-                    Log.i("MainActivity", "22222222222222222" ) ;
+
                     //将字符串转换成jsonObject对象
-                    JSONObject jsonObject = new JSONObject(String.valueOf(sb.append(line + "\n")));
+                    JSONObject jsonObject = new JSONObject(String.valueOf(sb.append(line )));
 //获取到json数据中里的Results数组内容
                     JSONArray resultJsonArray = jsonObject.getJSONArray("Results");
                     Log.e("MainActivity", String.valueOf(resultJsonArray) ) ;
 //                    bianli
+                    Map<String, Object> map=new HashMap<String, Object>();
                     for(int i=0;i<resultJsonArray.length();i++){
                         object = resultJsonArray.getJSONObject(i);
-
-                        Map<String, Object> map=new HashMap<String, Object>();
-                        Log.i("MainActivity", "55555555555555555555" ) ;
                         try {
                             //获取到json数据中的activity数组里的内容name
-                            String modelId = object.getString("Model_ID");
+                            String id = object.getString("Model_ID");
                             //获取到json数据中的activity数组里的内容startTime
-                            String modelName=object.getString("Model_Name");
-                            Log.i("MainActivity", "77777777777777777777777777777" ) ;
+                            String name=object.getString("Model_Name");
                             //存入map
-                            map.put("Model_ID", modelId);
-                            map.put("Model_Name", modelName);
+                            map.put("Model_ID", id);
+                            map.put("Model_Name", name);
                             //ArrayList集合
                             list.add(map);
-                            Log.e("MainActivity", String.valueOf(list) ) ;
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
+                    Log.e("MainActivity", String.valueOf(list) ) ;
                 }
-
-
-//                while ((line = reader.readLine()) != null)
-//                {
-//                    Log.i("MainActivity", "22222222222222222" ) ;
-//                    sb.append(line + "\n");
-//
-//                }
-//                String result = sb.toString(); //result is the whole string
-//                Log.i("MainActivity", "33333333333333333333" ) ;
-//                // convert string to JSON.
-//                JSONObject uvReport = new JSONObject(result);
-//                //get the double associated with "value"
-//                id= String.valueOf(uvReport.getInt("Model_ID"));
-////                name = String.valueOf(uvReport.getString("Model_Name"));
-////                name = String.valueOf(uvReport.getDouble("value"));
-//                name = uvReport.getString("Model_Name");
-
-
-
-
-                Log.i("MainActivity", "4444444444444444444" ) ;
-
             }
             catch (Exception e)
             {
             }
-            Log.i("HTTP", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             return "Done";
         }
-        public boolean fileExistance(String fname){
-            File file = getBaseContext().getFileStreamPath(fname);
-            return file.exists();   }
+
         //Type 2
         public void onProgressUpdate(Integer ... args)
         {
-//            progressBar();
+            Log.i("HTTP", "i22222222222222222222");
             Log.i("HTTP", "in onProgressUpdate");
         }
         //Type3
         public void onPostExecute(String fromDoInBackground)
         {
-//            modelID.setText(id);
-//            modelName.setText(name);
-//            Log.i("HTTP", fromDoInBackground);
-//            progressBar.setVisibility(View.INVISIBLE );
+            Log.i("HTTP", "i3333333333333333333333333333");
+
+            Log.i("HTTP", fromDoInBackground);
+            myAdapter.notifyDataSetChanged();
+            progressBar.setVisibility(View.INVISIBLE );
+//            Toast.makeText(this, "Inserted item id:"+ Integer.valueOf(modelID), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -215,18 +232,19 @@ public class SearchActivity extends AppCompatActivity {
 
             LayoutInflater inflater = getLayoutInflater();
             Object msg = getItem(position);
-
+            Log.i("MainActivity", "999999999999999999" ) ;
 //        make a new row in choose gender or send receive button
             View newRow = convertView;//msg.getGender()==0)
 
-            newRow = inflater.inflate(R.layout.activity_search, parent, false);
+            newRow = inflater.inflate(R.layout.search_list, parent, false);
 
             //finding what in the screen and set message into the new row
             TextView modelID = newRow.findViewById(R.id.modelID);
             modelID.setText( list.get(position).get("Model_ID").toString());
             TextView modelName = (TextView)newRow.findViewById(R.id.modelName);
             modelName.setText(list.get(position).get("Model_Name").toString());
-//
+            Log.i("MainActivity", "88888888888888888888888" ) ;
+
             return newRow;
         }
     }
