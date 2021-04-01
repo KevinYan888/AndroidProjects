962ca59247e4d98679fee0df71e95c98bf4c7c4e
Kefen <yan00105@algonquinlive.com>
2021-04-01 14:04:56 -0400

    project01 3-28

--- app/src/main/java/com/cst2335/project01/SearchActivity.java
+++ app/src/main/java/com/cst2335/project01/SearchActivity.java
@@ -1,20 +1,23 @@
 package com.cst2335.project01;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -27,33 +30,21 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import javax.net.ssl.HttpsURLConnection;
-
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener{
 //    ListView modelID;
 //    TextView modelID;
 //    TextView modelName;
@@ -64,6 +55,12 @@ public class SearchActivity extends AppCompatActivity {
     public static final String MAKE_NAME = "Make_Name";
     public static final String MODEL_NAME = "Model_Name";
     public static final String MODEL_ID = "Model_ID";
+
+    Intent intent = new Intent();
+
+    public static final int RESULT_CHAT =100;
+    public static final int RESULT_WEATHER =200;
+    public static final int RESULT_LOGIN =500;
 
     public  JSONObject object;
     public 	ListView lv;
@@ -195,15 +192,42 @@ public class SearchActivity extends AppCompatActivity {
             return true;
         });
 
+//      ImageButton starIcon = findViewById(R.id.starIcon);
+//        starIcon.setOnClickListener(e->{
+//            loadDataFromDatabase();
+//        });
+
         searchBtn.setOnClickListener(e->{
             String  searchE = searchEdit.getText().toString();
             if(!searchE.isEmpty()){
                 InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+//                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
                 Toast.makeText(this,"search for"+searchEdit.getText().toString()+"...",Toast.LENGTH_LONG);
                 list.clear();
             }
         });
+
+        setContentView(R.layout.activity_search);
+        //This gets the toolbar from the layout:
+        Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
+
+        //This loads the toolbar, which calls onCreateOptionsMenu below:
+        setSupportActionBar(tBar);
+
+//For NavigationDrawer:
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //For bottomNavigationBar
+        BottomNavigationView bnv = findViewById(R.id.bnv);
+        bnv.setOnNavigationItemSelectedListener(this);
 
     }
 
@@ -366,7 +390,7 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
-    CardataFragment cf = new CardataFragment();
+//    CardataFragment cf = new CardataFragment();
     private void loadDataFromDatabase()
     {
         //get a database connection:
@@ -398,4 +422,95 @@ public class SearchActivity extends AppCompatActivity {
 //            list.add(new CarListItems(name, make, id));
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.project_menu, menu);
+
+//        MenuInflater inflater0 = getMenuInflater();
+//        inflater0.inflate(R.menu.lab08_nmenu, menu);
+
+	    /* slide 15 material:
+	    MenuItem searchItem = menu.findItem(R.id.search_item);
+        SearchView sView = (SearchView)searchItem.getActionView();
+        sView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }  });
+
+	    */
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = null;
+        //Look at your menu XML file. Put a case for every id in that file:
+        switch(item.getItemId())
+        {
+            //what to do when the menu item is selected:
+            case R.id.item1:
+                message = "You clicked item 1";
+                break;
+            case R.id.search_item:
+                message = "You clicked on the search";
+                break;
+            case R.id.help_item:
+                message = "You clicked on help";
+                break;
+            case R.id.mail:
+                message = "You clicked on mail";
+                break;
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    // Needed for the OnNavigationItemSelected interface:
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item) {
+
+        String message = null;
+
+        switch(item.getItemId())
+        {
+            case R.id.chat:
+                message = "You clicked on Chat Page";
+
+                SearchActivity.this.setResult(RESULT_CHAT,intent);
+                Intent goToChat = new Intent(SearchActivity.this, CarActivity.class);
+                startActivity(goToChat);
+                break;
+            case R.id.search_item:
+                message = "You clicked on the search";
+
+                break;
+            case R.id.weather:
+                message = "You clicked on Weather Forecast";
+                SearchActivity.this.setResult(RESULT_WEATHER,intent);
+                Intent goToWeather = new Intent(SearchActivity.this,CarListItem.class);
+                startActivity(goToWeather);
+                break;
+            case R.id.login:
+                message = "You clicked on login page";
+                SearchActivity.this.setResult(RESULT_LOGIN,intent);
+                SearchActivity.this.finish();
+                break;
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        Toast.makeText(this, "NavigationDrawer: " + message, Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+
 }
